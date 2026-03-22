dfe6e4e356c397b62b3dd4946d829eb3bff5cd6c
Maria del Alba Encinas <mariadelalbae24@gmail.com>
2026-03-22 16:43:55 -0300

    agrego cosas

---
+++ node_modules/backend/src/prisma.config.ts
@@ -0,0 +1,16 @@
+// prisma.config.ts
+import 'dotenv/config';
+
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+
+  migrations: {
+    path: 'prisma/migrations',
+  },
+
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
